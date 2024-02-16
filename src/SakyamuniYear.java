aa448364bd0e97fc5362b7883a18c76a52a47512
Hwangseoeun <sunny030921@gmail.com>
2024-02-17 01:19:55 +0900

    18108번 - SakyamuniYear

---
+++ src/SakyamuniYear.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SakyamuniYear {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        Integer year = scanner.nextInt();
+
+        Integer gap = 2541 - 1998;
+
+        System.out.println(year - gap);
+    }
+}
