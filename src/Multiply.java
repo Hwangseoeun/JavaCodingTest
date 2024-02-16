d5bc57b91fbb43b251acbd0937a77e851c6a27c2
Hwangseoeun <sunny030921@gmail.com>
2024-02-17 00:58:53 +0900

    10998번 - Multiply

---
+++ src/Multiply.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Multiply {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        Integer A = scanner.nextInt();
+        Integer B = scanner.nextInt();
+
+        System.out.println(A * B);
+    }
+}
