05dfc6662b0e4d219d22515af671f2305f716749
Hwangseoeun <sunny030921@gmail.com>
2024-02-18 01:54:27 +0900

    ChildJeongmin Runtime error modify

---
+++ src/ChapterOne/ChildJeongmin.java
@@ -0,0 +1,27 @@
+package ChapterOne;
+
+import java.util.Scanner;
+
+public class ChildJeongmin {
+    public static void main(String[] args){
+/*        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLong();
+        String[] list = input.split(" ", 3);
+
+        long a = Long.parseLong(list[0]);
+        long b = Long.parseLong(list[1]);
+        long c = Long.parseLong(list[2]);
+
+        System.out.println(a + b + c);*/
+
+        Scanner scanner = new Scanner(System.in);
+
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        long c = scanner.nextLong();
+
+        System.out.println(a + b + c);
+
+    }
+}
