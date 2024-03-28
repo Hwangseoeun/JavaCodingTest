a56c9090cfc3671ef4ae7121edfae10cf6b2c760
Hwangseoeun <sunny030921@gmail.com>
2024-03-28 23:39:36 +0900

    Baekjoon_10951

---
+++ src/Baekjoon/Baekjoon_10951.java
@@ -0,0 +1,17 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon_10951 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNextInt()){
+
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+
+            System.out.println(A + B);
+        }
+    }
+}
