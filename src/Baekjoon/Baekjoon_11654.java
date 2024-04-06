bb706de7f3ea9de724209f29840dad42e5dd40f1
Hwangseoeun <sunny030921@gmail.com>
2024-04-06 21:23:39 +0900

    Baekjoon_11654

---
+++ src/Baekjoon/Baekjoon_11654.java
@@ -0,0 +1,15 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon_11654 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String N = scanner.next();
+
+        int num = N.hashCode();
+
+        System.out.println(num);
+    }
+}
