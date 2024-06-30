fdbeb64873f63eb2235252d4be906759a2d3279c
Hwangseoeun <sunny030921@gmail.com>
2024-06-30 16:03:40 +0900

    백준 10799번(Stack 클래스 이용)

---
+++ src/Baekjoon/Baekjoon_10799.java
@@ -0,0 +1,37 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Baekjoon_10799 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        int stick = 0;
+        char previous = ' ';
+
+        for(int i=0; i<input.length(); i++){
+            char current = input.charAt(i);
+
+            if(current == '('){
+                stack.push(current);
+            }
+            else{
+                stack.pop();
+                if(previous == ')'){
+                    stick += 1;
+                }
+                else{
+                    stick += stack.size();
+                }
+            }
+            previous = current;
+        }
+        System.out.println(stick);
+    }
+}
